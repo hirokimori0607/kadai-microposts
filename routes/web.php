d9b4e45fb3c4a755588ec5f185a76532f7a88b70
hiroki.mori <mori.hiro0607@outlook.com>
2020-04-26 02:15:30 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// ログイン認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
