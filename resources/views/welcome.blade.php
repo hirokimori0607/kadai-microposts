a2a3252ac9b458911c125d04f9c026733f4312f8
hiroki.mori <mori.hiro0607@outlook.com>
2020-05-13 13:33:30 +0000

    follow,unfollow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,22 +15,22 @@
             </aside>
             <div class="col-sm-8">
                 @if (Auth::id() == $user->id)
-                    {!! Form::open(['route' => 'microposts.store']) !!}
+                    {!! Form::open(['route' => 'tasks.store']) !!}
                         <div class="form-group">
                             {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
                             {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
                         </div>
                     {!! Form::close() !!}
                 @endif
-                @if (count($microposts) > 0)
-                    @include('microposts.microposts', ['microposts' => $microposts])
+                @if (count($tasks) > 0)
+                    @include('tasks.tasks', [tasks => $tasks])
                 @endif
             </div>
         </div>
     @else
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to the Microposts</h1>
+                <h1>Welcome to the Tasks</h1>
                 {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
